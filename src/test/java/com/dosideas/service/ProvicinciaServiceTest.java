a1333069328c55c7fc46512fc0c4f555c2619234
escuelita <escuelita@pd0208l.Tmoviles.com.ar>
2016-10-19 17:26:12 -0300

    Modificacion de inyeccion de dependencias.

--- src/test/java/com/dosideas/service/ProvicinciaServiceTest.java
+++ src/test/java/com/dosideas/service/ProvicinciaServiceTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.dosideas.service;
 
 import com.dosideas.domain.Provincia;
@@ -13,15 +8,11 @@ import static org.junit.Assert.assertNull;
 import org.junit.Before;
 import org.junit.Test;
 
-/**
- *
- * @author escuelita
- */
 public class ProvicinciaServiceTest {
     
     private ProvinciaService service;
     
-    @Before
+    @Before 
     public void setup() {
         service = new ProvinciaServiceImpl(new ProvinciaRepositoryImpl());
     }
